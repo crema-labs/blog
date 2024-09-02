e855bfa97cb5a8e92d4729b96c66948e28a20140
ayman <aymanshaik1015@gmail.com>
2024-09-02 19:02:31 +0530

    Quartz sync: Sep 2, 2024, 7:02 PM

---
+++ content/index.md
@@ -0,0 +1,6 @@
+---
+date: 2024-09-02
+title: Crema's Roastery
+---
+
+Hello, World!
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🍵 Crema's Roastery",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "blog.crema.sh",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {

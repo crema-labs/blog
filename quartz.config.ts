35e6fd3cfde5b797d285a9a9864e84001bf98674
ayman <aymanshaik1015@gmail.com>
2024-09-02 19:11:27 +0530

    Quartz sync: Sep 2, 2024, 7:11 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🍵 Crema's Roastery",
+    pageTitle: "🍵 Crema Labs",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
